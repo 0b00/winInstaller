6d1ed04678940ecdb636533ce3da601e7d912834
Arnav Jain <Jain10@arnav.jain.se>
2018-04-08 15:19:22 +0200

    Loop until correct input received Semicolons at end of statement Proper switch statement

--- install.ps1
+++ install.ps1
@@ -34,13 +34,14 @@ while($sysType -eq "") {
     Write-Output "  X - Exit"
     Write-Output "";
 
-    $sysType = read-host "Select profiles(s)";
-}
+$endloop = $False;
 
-$nums = $sysType.split(",").Trim();
+while (-Not ($endloop)) {
 
 $sysType = "";
-while($sysType -eq "") {
+    while ($sysType -eq "") {
+        Write-Output "";
+        Write-Output "";
     Write-Output "System Type";
     Write-Output "  Profiles:";
     Write-Output "  1 - Standard";
@@ -50,8 +51,8 @@ while($sysType -eq "") {
     Write-Output "  5 - Dev: Multimedia";
     Write-Output "";
     Write-Output "  Special:";
-    Write-Output "  A - Arnav"
-    Write-Output "  I - Ishan"
+        Write-Output "  A - Arnav";
+        Write-Output "  I - Ishan";
     Write-Output "";
     Write-Output "  X - Exit"
     Write-Output "";
@@ -59,29 +60,45 @@ while($sysType -eq "") {
     $sysType = read-host "Select profiles(s)";
 }
 
-$nums = $sysType.split(",").Trim();
+    $nums = $sysType.split(",");
 
 Write-Host $nums;
 
-#ForEach($answer in $nums){
-#
-#    switch ($answer) {
-#        1 {uninstall}
-#        2 {flashjava}
-#        3 {reader}
-#        4 {firefox}
-#        5 {chrome}
-#        6 {office}
-#        7 {reader}
-#        8 {updates}
-#        9 {exit}
-#        default {
-#            write-host -ForegroundColor red "Invalid Selection"
-#            sleep 2
-#            mainmenu
-#        }
-#    }
-#}
+    ForEach ($answer in $nums) {
+
+        # Required for Powershell 2.0
+        $answer = $answer.Trim();
+
+        $installtype = "";
+
+        switch ($answer) {
+            1 {$installtype = 'common'};
+            2 {$installtype = 'dev-common'};
+            3 {$installtype = 'dev-mobile'};
+            4 {$installtype = 'dev-doc'};
+            5 {$installtype = 'dev-media'};
+       
+            A {$installtype = 'sp-arnav'};
+            I {$installtype = 'sp-Ishan'};
+       
+            X {
+                Write-Host -ForegroundColor Yellow "Exiting";
+                $endloop = $True;
+                Exit;
+            }
+            default {
+                Write-Host -ForegroundColor red "Invalid Selection";
+                timeout 2;
+            }
+        }
+
+        if (-Not($installtype -eq "")) {
+            $endloop = $True;
+            Run-BoxStarter($installtype);
+        }
+
+    }
+}
 
 Write-Output "$profileDir/common.txt";
 
